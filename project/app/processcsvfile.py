5e378f49774b9fc2c3050b1cf9ec353bb884da1a
Sriram484 <116054870+Sriram484@users.noreply.github.com>
2024-10-04 13:54:50 +0530

    normal abnormal push

--- project/app/processcsvfile.py
+++ project/app/processcsvfile.py
@@ -102,7 +102,7 @@ def process_and_predict(filepath):
     # Load the files as DataFrames
     first=filepath[0]
     second=filepath[1]
-    print(first,second)
+    # print(first,second)
     fhr_data = pd.read_csv(first['file_path'])
     uct_data = pd.read_csv(second['file_path'])
 
@@ -126,7 +126,7 @@ def process_and_predict(filepath):
     features = extract_features(fhr, uct, window_size=60)
     feature_names = ['LB', 'AC', 'FM', 'UC', 'DL', 'DS', 'DP', 'ASTV', 'MSTV', 'ALTV', 'MLTV', 'Width', 'Min', 'Max', 'Nmax', 'Nzeros', 'Mode', 'Mean', 'Median', 'Variance', 'Tendency']
     features_df = pd.DataFrame(features, columns=feature_names)
-    print(features_df)
+    # print(features_df)
 
     # Load model and make prediction
     model = load_model()
@@ -134,10 +134,13 @@ def process_and_predict(filepath):
     
     predictions.append(prediction)
     predictions_array = np.array(predictions)
+    a= "Normal" if first['filename'].startswith("Normal") else "Abnormal"
+    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$",first)
+    
     
     # Get the predicted condition and recommendations (optional part of your code)
     r = predict_and_recommend(predictions_array)
     predicted_condition, recommended_medicine, suggested_diet = r
-    print(predicted_condition, recommended_medicine, suggested_diet)
+    # print(predicted_condition, recommended_medicine, suggested_diet)
     
-    return predictions_array, features_df, predicted_condition, recommended_medicine, suggested_diet, image_base64
+    return predictions_array, features_df, predicted_condition, recommended_medicine, suggested_diet, image_base64,a
