0b34a5c6c0c0880ba82340ec112380e0cdcafbda
ROKHITHRAN <118052257+ROKHITHRAN@users.noreply.github.com>
2024-10-04 07:39:32 +0530

    delete

--- project/app/trainedmodel1.py
+++
@@ -1,7 +0,0 @@
-import joblib
-import os
-def load_model():
-    """Function to load the trained model."""
-    model_path = os.path.join(os.path.dirname(__file__), 'multiclassifiertuned1trained.pkl')
-    model = joblib.load(model_path)
-    return model
